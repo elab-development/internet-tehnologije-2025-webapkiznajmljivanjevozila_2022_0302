b3fedd35f7948371ec30c14e5d97c1482212cc60
Kristijan Plavsic <kplavsic4@gmail.com>
2026-01-30 14:44:15 +0100

    Connect owner dashboard to real API data

--- RentalCar/client/src/pages/owner/Dashboard.jsx
+++ RentalCar/client/src/pages/owner/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { dummyDashboardData, assets } from "../../assets/assets";
 import Title from "../../components/owner/Title";
+import { useAppContext } from "../../context/AppContext";
 
 const Dashboard = () => {
-  const currency = import.meta.env.VITE_CURRENCY;
+
+  const {axios, isOwner, currency} = useAppContext()
 
   const [data, setData] = useState({
     totalCars: 0,
@@ -35,11 +37,26 @@ const Dashboard = () => {
       value: data.completedBookings,
       icon: assets.listIconColored,
     },
-  ];
+  ]
+
+  const fetchDashboardData = async ()=>{
+    try {
+      const {data} = await axios.get('/api/owner/dashboard')
+      if (data.success){
+        setData(data.dashboardData)
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
 
   useEffect(() => {
-    setData(dummyDashboardData);
-  }, []);
+    if(isOwner){
+      fetchDashboardData()
+    }
+  }, [isOwner]);
 
   return (
     <div className="px-4 pt-10 md:px-10 flex-1">
